c44b0de4e1df1d7457dd476cb2e56b4835c85671
KlentParaiso <114377523+KlentParaiso@users.noreply.github.com>
2025-05-20 07:05:17 +0800

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -25,7 +25,7 @@ const Home: React.FC = () => {
       <div className="relative rounded-xl overflow-hidden shadow-xl mb-12 aspect-video border-2 border-primary-300 bg-primary-100">
         <iframe
           className="w-full h-full"
-          src="https://www.youtube.com/watch?v=roaBNInEirc"
+          src="https://www.youtube.com/embed/roaBNInEirc"
           title="YouTube video player"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
